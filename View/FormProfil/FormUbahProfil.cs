5adaa1ee246ae9527eea6b181b1ae8957f4faf88
dyaksaadi <168102395+dyaksaadi@users.noreply.github.com>
2024-11-21 10:55:18 +0700

    Update FormUbahProfil.cs

--- View/FormProfil/FormUbahProfil.cs
+++ View/FormProfil/FormUbahProfil.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+// ﻿using System;
+// using System.Collections.Generic;
+// using System.ComponentModel;
+// using System.Data;
+// using System.Drawing;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
+// using System.Windows.Forms;
 
-namespace PBOBarberMate.View
-{
-    public partial class FormUbahProfil : Form
-    {
-        public FormUbahProfil()
-        {
-            InitializeComponent();
-        }
+// namespace PBOBarberMate.View
+// {
+//     public partial class FormUbahProfil : Form
+//     {
+//         public FormUbahProfil()
+//         {
+//             InitializeComponent();
+//         }
 
-        private void btnKembali_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-    }
-}
+//         private void btnKembali_Click(object sender, EventArgs e)
+//         {
+//             this.Close();
+//         }
+//     }
+// }
