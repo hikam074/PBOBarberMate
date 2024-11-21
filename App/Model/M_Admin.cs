add048f330ac8d404f7673dc74565ac6cda128a1
Mohammad Al Hikam <141762236+hikam074@users.noreply.github.com>
2024-11-22 00:04:37 +0700

    Update M_Admin.cs manually

--- App/Model/M_Admin.cs
+++ App/Model/M_Admin.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -22,7 +22,7 @@ namespace PBOBarberMate.App.Model
         public M_Admin(string email, string password) : base(email, password)
         {
             // assign role otomatis
-            this.role = role;
+            this.role = AkunRole.Admin;
         }
         //public void manageInventaris(Inventaris)
     }
