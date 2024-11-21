cf27ba556820a58aa25c445f364eb1ac3668316f
dyaksaadi <168102395+dyaksaadi@users.noreply.github.com>
2024-11-21 10:43:07 +0700

    Update FormUbahProfil.Designer.cs

--- View/FormProfil/FormUbahProfil.Designer.cs
+++ View/FormProfil/FormUbahProfil.Designer.cs
@@ -1,60 +1,60 @@
-// ﻿namespace PBOBarberMate.View
-// {
-//     partial class FormUbahProfil
-//     {
-//         /// <summary>
-//         /// Required designer variable.
-//         /// </summary>
-//         private System.ComponentModel.IContainer components = null;
+﻿namespace PBOBarberMate.View
+{
+    partial class FormUbahProfil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-//         /// <summary>
-//         /// Clean up any resources being used.
-//         /// </summary>
-//         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-//         protected override void Dispose(bool disposing)
-//         {
-//             if (disposing && (components != null))
-//             {
-//                 components.Dispose();
-//             }
-//             base.Dispose(disposing);
-//         }
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-//         #region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-//         /// <summary>
-//         /// Required method for Designer support - do not modify
-//         /// the contents of this method with the code editor.
-//         /// </summary>
-//         private void InitializeComponent()
-//         {
-//             btnKembali = new Button();
-//             SuspendLayout();
-//             // 
-//             // btnKembali
-//             // 
-//             btnKembali.Location = new Point(12, 12);
-//             btnKembali.Name = "btnKembali";
-//             btnKembali.Size = new Size(75, 39);
-//             btnKembali.TabIndex = 0;
-//             btnKembali.Text = "Kembali";
-//             btnKembali.UseVisualStyleBackColor = true;
-//             btnKembali.Click += btnKembali_Click;
-//             // 
-//             // FormUbahProfil
-//             // 
-//             AutoScaleDimensions = new SizeF(7F, 15F);
-//             AutoScaleMode = AutoScaleMode.Font;
-//             ClientSize = new Size(800, 450);
-//             Controls.Add(btnKembali);
-//             Name = "FormUbahProfil";
-//             StartPosition = FormStartPosition.CenterScreen;
-//             Text = "BarberMate : Ubah Profil";
-//             ResumeLayout(false);
-//         }
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnKembali = new Button();
+            SuspendLayout();
+            // 
+            // btnKembali
+            // 
+            btnKembali.Location = new Point(12, 12);
+            btnKembali.Name = "btnKembali";
+            btnKembali.Size = new Size(75, 39);
+            btnKembali.TabIndex = 0;
+            btnKembali.Text = "Kembali";
+            btnKembali.UseVisualStyleBackColor = true;
+            btnKembali.Click += btnKembali_Click;
+            // 
+            // FormUbahProfil
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnKembali);
+            Name = "FormUbahProfil";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "BarberMate : Ubah Profil";
+            ResumeLayout(false);
+        }
 
-//         #endregion
+        #endregion
 
-//         private Button btnKembali;
-//     }
-// }
+        private Button btnKembali;
+    }
+}
