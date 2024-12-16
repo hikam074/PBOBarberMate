677f08fc7ae40f9bb52caba8dfec7fffba1b608e
dyaksaadi <168102395+dyaksaadi@users.noreply.github.com>
2024-12-16 18:12:53 +0700

    Add files via upload

---
+++ View/FormKelolaKaryawan/FiturPresensiKaryawan.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace PBOBarberMate.View.FormKelolaKaryawan
+{
+    partial class FiturPresensiKaryawan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            tbIDAKUNFiturPresensiKaryawan = new TextBox();
+            tbIDSHIFTFiturPresensiKaryawan = new TextBox();
+            dtpFiturPresensiKaryawan = new DateTimePicker();
+            btnBatalFiturPresensiKaryawan = new Button();
+            btnSelesaiFiturPresensiKaryawan = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(68, 72);
+            label1.Name = "label1";
+            label1.Size = new Size(53, 15);
+            label1.TabIndex = 0;
+            label1.Text = "ID AKUN";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(68, 109);
+            label2.Name = "label2";
+            label2.Size = new Size(51, 15);
+            label2.TabIndex = 1;
+            label2.Text = "ID SHIFT";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(68, 146);
+            label3.Name = "label3";
+            label3.Size = new Size(100, 15);
+            label3.TabIndex = 2;
+            label3.Text = "WAKTU PRESENSI";
+            // 
+            // tbIDAKUNFiturPresensiKaryawan
+            // 
+            tbIDAKUNFiturPresensiKaryawan.Location = new Point(187, 69);
+            tbIDAKUNFiturPresensiKaryawan.Name = "tbIDAKUNFiturPresensiKaryawan";
+            tbIDAKUNFiturPresensiKaryawan.Size = new Size(100, 23);
+            tbIDAKUNFiturPresensiKaryawan.TabIndex = 3;
+            // 
+            // tbIDSHIFTFiturPresensiKaryawan
+            // 
+            tbIDSHIFTFiturPresensiKaryawan.Location = new Point(187, 106);
+            tbIDSHIFTFiturPresensiKaryawan.Name = "tbIDSHIFTFiturPresensiKaryawan";
+            tbIDSHIFTFiturPresensiKaryawan.Size = new Size(100, 23);
+            tbIDSHIFTFiturPresensiKaryawan.TabIndex = 4;
+            // 
+            // dtpFiturPresensiKaryawan
+            // 
+            dtpFiturPresensiKaryawan.Location = new Point(187, 140);
+            dtpFiturPresensiKaryawan.Name = "dtpFiturPresensiKaryawan";
+            dtpFiturPresensiKaryawan.Size = new Size(200, 23);
+            dtpFiturPresensiKaryawan.TabIndex = 6;
+            // 
+            // btnBatalFiturPresensiKaryawan
+            // 
+            btnBatalFiturPresensiKaryawan.Location = new Point(531, 399);
+            btnBatalFiturPresensiKaryawan.Name = "btnBatalFiturPresensiKaryawan";
+            btnBatalFiturPresensiKaryawan.Size = new Size(75, 23);
+            btnBatalFiturPresensiKaryawan.TabIndex = 7;
+            btnBatalFiturPresensiKaryawan.Text = "BATAL";
+            btnBatalFiturPresensiKaryawan.UseVisualStyleBackColor = true;
+            btnBatalFiturPresensiKaryawan.Click += btnBatalFiturPresensiKaryawan_Click;
+            // 
+            // btnSelesaiFiturPresensiKaryawan
+            // 
+            btnSelesaiFiturPresensiKaryawan.Location = new Point(612, 399);
+            btnSelesaiFiturPresensiKaryawan.Name = "btnSelesaiFiturPresensiKaryawan";
+            btnSelesaiFiturPresensiKaryawan.Size = new Size(75, 23);
+            btnSelesaiFiturPresensiKaryawan.TabIndex = 8;
+            btnSelesaiFiturPresensiKaryawan.Text = "SELESAI";
+            btnSelesaiFiturPresensiKaryawan.UseVisualStyleBackColor = true;
+            btnSelesaiFiturPresensiKaryawan.Click += btnSelesaiFiturPresensiKaryawan_Click;
+            // 
+            // FiturPresensiKaryawan
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSelesaiFiturPresensiKaryawan);
+            Controls.Add(btnBatalFiturPresensiKaryawan);
+            Controls.Add(dtpFiturPresensiKaryawan);
+            Controls.Add(tbIDSHIFTFiturPresensiKaryawan);
+            Controls.Add(tbIDAKUNFiturPresensiKaryawan);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "FiturPresensiKaryawan";
+            Text = "EditPresensiKaryawan";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox tbIDAKUNFiturPresensiKaryawan;
+        private TextBox tbIDSHIFTFiturPresensiKaryawan;
+        private DateTimePicker dtpFiturPresensiKaryawan;
+        private Button btnBatalFiturPresensiKaryawan;
+        private Button btnSelesaiFiturPresensiKaryawan;
+    }
+}
---
+++ View/FormKelolaKaryawan/FiturPresensiKaryawan.cs
@@ -0,0 +1,102 @@
+﻿using PBOBarberMate.App.Context;
+using PBOBarberMate.App.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PBOBarberMate.View.FormKelolaKaryawan
+{
+    public partial class FiturPresensiKaryawan : Form
+    {
+        public bool IsEditMode { get; set; } = false;
+        public int idPresensi { get; set; }
+
+        public FiturPresensiKaryawan()
+        {
+            InitializeComponent();
+            UpdateButtonText();
+        }
+
+        private void btnSelesaiFiturPresensiKaryawan_Click(object sender, EventArgs e)
+        {
+            if (!ValidateInput())
+            {
+                MessageBox.Show("Data Harus Lengkap Mohon Periksa Kembali");
+                return;
+            }
+            M_Presensi presensi = new M_Presensi
+            {
+                id_akun = int.Parse(tbIDAKUNFiturPresensiKaryawan.Text),
+                id_shift = int.Parse(tbIDSHIFTFiturPresensiKaryawan.Text),
+                waktu_presensi = dtpFiturPresensiKaryawan.Value,
+            };
+            
+            if (IsEditMode)
+            {
+                PresensiContext.UpdatePresensi(presensi);
+                MessageBox.Show("Jadwal berhasil diubah");
+            }
+            else
+            {
+                PresensiContext.AddPresensi(presensi);
+                MessageBox.Show("Jadwal berhasil ditambahkan");
+            }
+
+            ClearFields();
+
+            this.DialogResult = DialogResult.OK;
+            this.Hide();
+            FormKelolaKaryawan formKelolaKaryawan = new FormKelolaKaryawan();
+            formKelolaKaryawan.ShowDialog();
+        }
+
+
+
+        private bool ValidateInput()
+        {
+            if (string.IsNullOrWhiteSpace(tbIDAKUNFiturPresensiKaryawan.Text) ||
+                string.IsNullOrWhiteSpace(tbIDSHIFTFiturPresensiKaryawan.Text) 
+                )
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void ClearFields()
+        {
+            tbIDAKUNFiturPresensiKaryawan.Clear();
+            tbIDSHIFTFiturPresensiKaryawan.Clear();
+            dtpFiturPresensiKaryawan.Value = dtpFiturPresensiKaryawan.MinDate; 
+            // Mengatur ke tanggal minimum
+        }
+
+        private void btnBatalFiturPresensiKaryawan_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Hide();
+        }
+        public void PopulateForm(M_Presensi karyawan)
+        {
+            tbIDAKUNFiturPresensiKaryawan.Text = karyawan.id_akun.ToString();
+            tbIDSHIFTFiturPresensiKaryawan.Text = karyawan.id_shift.ToString();
+            dtpFiturPresensiKaryawan.Value = karyawan.waktu_presensi;
+            IsEditMode = true;
+            idPresensi = karyawan.id_presensi;
+            UpdateButtonText();
+        }
+
+        private void UpdateButtonText()
+        {
+            btnSelesaiFiturPresensiKaryawan.Text = IsEditMode ? "Update" : "Add";
+        }
+        
+    }
+}
