d218b32fd3bb960863484934b4bd76d76a402d81
dyaksaadi <168102395+dyaksaadi@users.noreply.github.com>
2024-12-16 08:39:30 +0700

    Add files via upload

---
+++ App/Model/M_Presensi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace PBOBarberMate.App.Model
+{
+    internal class M_Presensi
+    {
+        [Key]
+        public int id_presensi { get; set; }
+        public DataSetDateTime timestamp { get; set; }
+        
+    }
+}
