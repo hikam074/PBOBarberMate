d01d3f9e52f190b83c58e648b14f3af65c2d30a9
dyaksaadi <168102395+dyaksaadi@users.noreply.github.com>
2024-12-18 12:52:49 +0700

    Update M_Presensi.cs

--- App/Model/M_Presensi.cs
+++ App/Model/M_Presensi.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,10 @@ namespace PBOBarberMate.App.Model
     public class M_Presensi
     {
         public int id_presensi { get; set; }
+        public string nama_akun { get; set; }
         public int id_akun { get; set; }
         public int id_shift { get; set; }
+        public string hari {  get; set; }
         public DateTime waktu_presensi { get; set; }
-        public DataSetDateTime timestamp { get; set; }
     }
 }
